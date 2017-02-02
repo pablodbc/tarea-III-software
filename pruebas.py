b29aeb604923dbef5ac2752452a27eb3a4dbacdf
Pablo <pablodbc30@gmail.com>
2017-02-01 22:55:45 -0400

    Primera prueba: Cedula negativa. Agregada verificacion al main

--- pruebas.py
+++ pruebas.py
@@ -2,17 +2,18 @@
 Created on 1 feb. 2017
 
 @author: carlos
+@author: pablo
 '''
 import unittest
+from main import *
 
 
 class Test(unittest.TestCase):
 
 
-    def testName(self):
-        pass
+    def testCedulaNegativa(self):
+        billetera = BilleteraElectronica(0,"Pablo","Betancourt",-1,1234)
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
