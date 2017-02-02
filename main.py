ac85222bf55486df8376bfe7ccc8f83cf6830ed7
cserradag96 <cserradag96@gmail.com>
2017-02-01 21:13:08 -0400

    Proyecto creado

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+Created on 1 feb. 2017
+
+@author: carlos
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ pruebas.py
@@ -0,0 +1,18 @@
+'''
+Created on 1 feb. 2017
+
+@author: carlos
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
