d697189121d82fb7930c4b36cefc0b1cf5159f5a
cserradag96 <cserradag96@gmail.com>
2017-02-01 22:03:01 -0400

    Clase BilleteraElectronica creada

--- main.py
+++ main.py
@@ -4,5 +4,44 @@ Created on 1 feb. 2017
 @author: carlos
 '''
 
+class Registro:
+    def __init__(self, monto, fecha, localID):
+        self.monto = monto
+        self.fecha = fecha
+        self.localID = localID
+
+    def __str__(self):
+        return "\nMonto: "+str(self.monto)+"\nFecha: "+str(self.fecha)+"\nEstablecimiento: "+str(self.localID)+"\n"
+
+class BilleteraElectronica:
+    def __init__(self, identificador, nombres, apellidos, CI, PIN, balance = 0):
+        self.identificador =identificador
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.CI = CI
+        self.PIN = PIN
+        self.recargas = []
+        self.consumos = []
+        self.balance = balance
+
+    def recargar(self, monto, fecha, localID):
+        self.recargas += [Registro(monto, fecha, localID)]
+        self.balance += monto
+
+    def consumir(self, PIN, monto, fecha, localID):
+        if self.PIN == PIN:
+            if self.balance >= monto:
+                self.recargas += [Registro(monto, fecha, localID)]
+                self.balance -= monto
+
+            else:
+                print("No hay suficiente crédito para llevar acabo la operación")
+
+        else:
+            print("El número de PIN no coincide con el registradoen la billetera")
+
+    def saldo(self):
+        return self.balance
+
 if __name__ == '__main__':
     pass
